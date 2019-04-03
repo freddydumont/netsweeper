9830c90083f597297134fd59aba2d5e2157ed613
Frederick Morin <frederickmorin18@gmail.com>
2019-04-03 14:02:24 +1300

    Add centerOnScreen util

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -20,3 +20,22 @@ export enum Tiles {
   SEVEN,
   EIGHT,
 }
+
+/**
+ * Centers an object on the scene.
+ * @param camera scene main camera
+ * @param width width of the object
+ * @param height height of the object
+ * @returns centered coordinates
+ */
+export function centerOnScreen(
+  camera: Phaser.Cameras.Scene2D.Camera,
+  width: number,
+  height: number
+): { x: number; y: number } {
+  const { centerX, centerY } = camera;
+  return {
+    x: centerX - width / 2,
+    y: centerY - height / 2,
+  };
+}
