856fcbf0860d46f6bf7d98a5f65a699c0dc349bb
Frederick Morin <frederickmorin18@gmail.com>
2019-04-19 12:33:17 +1200

    Revert "Preload images in phaser"
    This reverts commit cba697b7a37f697638ec7fee0996b72a48e4e064.

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -1,8 +1,5 @@
 import tiles from '../../assets/minesweeper.png';
 import digits from '../../assets/digits.png';
-import spinthink from '../../assets/spinthink.gif';
-import gasp from '../../assets/gasp.png';
-import dead from '../../assets/dead.png';
 
 export default class PreloadScene extends Phaser.Scene {
   constructor() {
@@ -31,21 +28,6 @@ export default class PreloadScene extends Phaser.Scene {
       frameHeight: 46,
       frameWidth: 26,
     });
-
-    this.load.image([
-      {
-        key: 'think',
-        url: spinthink,
-      },
-      {
-        key: 'gasp',
-        url: gasp,
-      },
-      {
-        key: 'dead',
-        url: dead,
-      },
-    ]);
 
     this.load.on('complete', function() {
       loadingText.destroy();
